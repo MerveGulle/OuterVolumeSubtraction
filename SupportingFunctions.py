84011c7f1bd5697cb80c69c691339b7d632fb548
Merve Gulle <glle0001@umn.edu>
2022-09-14 13:42:59 -0500

    Add g-factor map calculator

--- SupportingFunctions.py
+++ SupportingFunctions.py
@@ -15,25 +15,27 @@ def A(x, Smaps, mask):
 def AT(x, Smaps):
     return np.sum(kspace_to_im(x)*np.conj(Smaps), 2)
 
-def cgsense(kspace,Smaps,mask,max_iter=20):
+def cgsense(kspace,Smaps,mask,max_iter=50):
     a = AT(kspace,Smaps)
     p = np.copy(a)
     r_now = np.copy(a)
     xn = np.zeros_like(a)
     for i in np.arange(max_iter):
-        # q = (EHE)p
-        q = AT(A(p,Smaps,mask),Smaps)
-        # rr_pq = r'r/p'q
-        rr_pq = np.sum(r_now*np.conj(r_now))/np.sum(q*np.conj(p))
-        xn = xn + rr_pq * p
-        r_next = r_now - rr_pq * q
-        # p = r_next + r_next'r_next/r_now'r_now
-        p = r_next + (np.sum(r_next*np.conj(r_next))/np.sum(r_now*np.conj(r_now))) * p
-        r_now = np.copy(r_next)
+        delta = np.sum(r_now*np.conj(r_now))/np.sum(a*np.conj(a))
+        if delta > 1e-4:
+            # q = (EHE)p
+            q = AT(A(p,Smaps,mask),Smaps)
+            # rr_pq = r'r/p'q
+            rr_pq = np.sum(r_now*np.conj(r_now))/np.sum(q*np.conj(p))
+            xn = xn + rr_pq * p
+            r_next = r_now - rr_pq * q
+            # p = r_next + r_next'r_next/r_now'r_now
+            p = r_next + (np.sum(r_next*np.conj(r_next))/np.sum(r_now*np.conj(r_now))) * p
+            r_now = np.copy(r_next)
     return xn
 
 
-def ADMM(kspace,Smaps,mask,mu=1e-2,lmbda=5e-7,max_iter=20,cg_max_iter=8):
+def ADMM(kspace,Smaps,mask,mu=1e-2,lmbda=1,max_iter=20,cg_max_iter=8):
     # initialization
     xj = rssq(kspace_to_im(kspace))
     SHFHd = AT(kspace, Smaps)
@@ -60,4 +62,37 @@ def ADMM(kspace,Smaps,mask,mu=1e-2,lmbda=5e-7,max_iter=20,cg_max_iter=8):
         # update nu1(j)
         nu1j = nu1j - (u1j - xj)
     return xj
-    
+
+
+def gfactor(Smaps, R):
+    Nx, Ny, Nc = Smaps.shape
+    gmap = np.zeros((Nx,Ny), dtype=complex)
+    for x in np.arange(Nx):
+        for y in np.arange(Ny):
+            C = np.zeros((1,R,Nc), dtype=complex)
+            ind_mat = np.zeros((1,8), dtype=complex)
+            for Cy in np.arange(R):
+                cur_x = x
+                cur_y = np.mod(y+(Cy*Ny)//R, Ny)
+                C[0,Cy,:] = Smaps[cur_x, cur_y, :]
+                ind_mat[0,Cy] = x * Ny + y
+            C = C.reshape(R,Nc)
+            zero_col = np.argwhere(np.all(C[...,:]!=0, axis=0))
+            np.delete(C, zero_col, axis=1)
+            if C.shape[1]==0:
+                gmap[x,y] = 0
+            else:
+                CtC = np.matmul(np.conj(C).T,C)
+                CtCi = np.linalg.pinv(CtC)
+                gmap[x,y] = np.sqrt(CtC[0,0]*CtCi[0,0])
+    return gmap
+
+
+
+
+
+
+
+
+
+

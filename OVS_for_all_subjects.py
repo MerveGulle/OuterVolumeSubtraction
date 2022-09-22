7c25eb9896e4dc2bf6e17191dcd26ad6586abc5e
Merve Gulle <glle0001@umn.edu>
2022-09-22 16:06:00 -0500

    Load the data

--- OVS_for_all_subjects.py
+++ OVS_for_all_subjects.py
@@ -6,3 +6,19 @@ from espirit import espirit
 from scipy.io import loadmat
 
 
+# %% Data Loading
+# Load one slice data
+subject_number = 3
+filename = "subject" + str(subject_number) + ".mat"
+realtime_data = mat73.loadmat(filename)
+# Images for each time frame, created with TGRAPPA --> [Nx(full), Ny(full), Ndyn]
+_, im_tgrappa_full = list(realtime_data.items())[0]
+# Kspace data (partial) --> [Nx(full), Ny(full), Nc, Ndyn]
+_, datas = list(realtime_data.items())[1]
+datas = datas[68:320,0:72,:,:]*1e6
+datas = datas.astype('complex64')
+
+Nx = datas.shape[0]             # num of RO pixels (vertical)
+Ny = datas.shape[1]             # num of PE pixels (horizontal)
+Nc = datas.shape[2]             # num of coils
+Ndyn = datas.shape[3]           # num of time frames

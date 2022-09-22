249380767c6f4b0cc24d02374ec08863f713e0a2
Merve Gulle <glle0001@umn.edu>
2022-09-22 16:23:29 -0500

    Check cardiac phases vs time frames

--- OVS_for_all_subjects.py
+++ OVS_for_all_subjects.py
@@ -22,3 +22,19 @@ Nx = datas.shape[0]             # num of RO pixels (vertical)
 Ny = datas.shape[1]             # num of PE pixels (horizontal)
 Nc = datas.shape[2]             # num of coils
 Ndyn = datas.shape[3]           # num of time frames
+
+
+# %% images for the first 24 time frames
+num_img = 24
+    
+figure = plt.figure(figsize=(15,12))
+for tf in np.arange(num_img):
+    plt.subplot(3,num_img//3,tf+1)
+    plt.imshow(np.abs(im_tgrappa_full[123:213,23:103,tf]),cmap="gray",vmax=0.2)
+    plt.title('TF #'+f'{tf+1}',fontsize=12)
+    plt.axis("off")
+    
+    
+    
+    
+    

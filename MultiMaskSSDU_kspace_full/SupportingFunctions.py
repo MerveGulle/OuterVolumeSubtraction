e208def2b0490a38aa90c73483553a1043b8b33f
Merve Gulle <glle0001@umn.edu>
2022-11-10 16:28:26 -0600

    init: Initiate the train code with main structures

--- MultiMaskSSDU_kspace_full/SupportingFunctions.py
+++ MultiMaskSSDU_kspace_full/SupportingFunctions.py
@@ -7,7 +7,7 @@ def fft2 (image, axis=[1,2]):
 
 # image = ifft2(kspace): iFFT of n-slice kspace to image: [n Nx Ny Nc] --> [n Nx Ny]
 def ifft2 (kspace, axis=[1,2]):
-    return torch.fft.ifftshift(torch.fft.ifftn(torch.fft.fftshift(kspace, dim=axis), dim=axis, norm='ortho'), dim=axis)
+    return torch.fft.fftshift(torch.fft.ifftn(torch.fft.ifftshift(kspace, dim=axis), dim=axis, norm='ortho'), dim=axis)
 
 # kspace = forward(image): encoding one slice image to kspace: [2 Nx Ny] --> [1 Nx Ny Nc]
 # Smaps: sensitivity map [2 Nx Ny Nc]
---
+++ MultiMaskSSDU_kspace_full/train.py
@@ -0,0 +1,15 @@
+### HYPERPARAMETERS
+
+### PATHS 
+
+# 0) Fix randomness for reproducible experiment
+
+# 1) Device configuration
+
+# 2) Load Data
+
+# 3) Create Model structure
+
+# 4) Start Training
+
+# 5) Plot the Loss Graph

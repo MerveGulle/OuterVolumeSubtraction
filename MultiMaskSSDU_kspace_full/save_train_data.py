3e08d1b815a0f22d69767c45cf73e07ff33f8854
Merve Gulle <glle0001@umn.edu>
2022-11-10 13:34:23 -0600

    feat: Read the kspace data

--- MultiMaskSSDU_kspace_full/save_train_data.py
+++ MultiMaskSSDU_kspace_full/save_train_data.py
@@ -7,6 +7,7 @@ import mat73
 K = 5        # number of masks
 acs_size = 8 # DC calibration region size
 rho = 0.6    # data consistency / whole mask
+
 # get data indices
 data_indices = loadmat('data_indices.mat')
 _, indices = list(data_indices.items())[3]
@@ -15,6 +16,7 @@ _, indices = list(data_indices.items())[3]
 # indices[1]: slice numbers
 # indices[2]: time frame numbers (number of slices x 3)
 
+os.chdir('/home/daedalus1-raid1/akcakaya-group-data/ScannerData/Data/Volunteer/RealTime_fromPK/database_short_axis')
 number_of_subjects = indices.shape[1]
 sub_counter = 0
 for sub in np.arange(number_of_subjects):
@@ -24,10 +26,13 @@ for sub in np.arange(number_of_subjects):
     for slc in indices[1,sub][0]:
         print('slice number = '+ f'{slc}')
         
-        os.chdir('/home/daedalus1-raid1/akcakaya-group-data/ScannerData/Data/Volunteer/RealTime_fromPK/database_short_axis')
+        # read the data
         filename = "subject_" + str(sub) + "_slice_" + str(slc) + ".mat"
         filename = "subject_" + str(sub) + "_slice_" + str(slc) + ".mat"
         realtime_data = mat73.loadmat(filename)
+        
+        _, kspace_data = list(realtime_data.items())[1]
+        
         
         slc_counter += 1
     sub_counter += 1

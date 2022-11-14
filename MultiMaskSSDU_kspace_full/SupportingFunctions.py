fix: Debug validation loss calculation

MultiMaskSSDU_kspace_full/SupportingFunctions.py
@@ -85,11 +85,17 @@ class OVS_DatasetValidation():
         self.composite_kspace = torch.from_numpy(slice_data['composite_kspace'])
         self.sense_maps = torch.from_numpy(slice_data['sense_maps'])
         self.acc_mask = torch.from_numpy(slice_data['acc_mask'])
+        self.data_consistency_masks = torch.from_numpy(slice_data['data_consistency_masks'])
         self.sub_slc_tf = torch.from_numpy(slice_data['sub_slc_tf'])
-        self.x0 = backward(self.composite_kspace*self.acc_mask[...,None], self.sense_maps)
+        Nx, Ny = self.acc_mask.shape
+        K = (self.data_consistency_masks).shape[2]
+        acc_kspace = self.composite_kspace[...,None]*self.data_consistency_masks[None,:,:,None,:]
+        self.x0 = torch.zeros([2*K,Nx,Ny], dtype=torch.complex64)
+        for k in range(K):
+            self.x0[2*k:2*k+2] = backward(acc_kspace[...,k],self.sense_maps)
         
-        return self.x0, self.composite_kspace, self.sense_maps, self.acc_mask, self.sub_slc_tf, index
-        
+        return self.x0, self.composite_kspace, self.sense_maps, self.acc_mask, self.data_consistency_masks, self.sub_slc_tf, index
+    
     def __len__(self):
         return self.num_slice 
   

MultiMaskSSDU_kspace_full/train.py
@@ -100,22 +100,28 @@ for epoch in range(params['num_epoch']):
         # Optimize
         optimizer.step()
 
-    for i, (x0, composite_kspace, sense_map, acc_mask, sub_slc_tf, index) in enumerate(validation_loader['valid_loader']):
-        x0                     = x0[0].to(device)                       # [2,Nx,Ny]
-        composite_kspace       = composite_kspace[0].to(device)         # [1,Nx,Ny,Nc]
-        sense_map              = sense_map[0].to(device)                # [2,Nx,Ny,Nc]
-        acc_mask               = acc_mask[0].to(device)                 # [Nx,Ny]
-        # Forward pass
-        xt = torch.clone(x0)
-        for t in range(params['T']):
-            L, zt = denoiser(xt[None,...])
-            xt = model.DC_layer(x0,zt[0],L,sense_map,acc_mask)
-            
-        # loss calculation for kth mask
-        kspace_loss = sf.forward(xt, sense_map, acc_mask)
-        # loss calculation
-        loss = L1L2Loss(kspace_loss, composite_kspace*acc_mask[None,...,None])
-        loss_arr_valid[epoch] += loss.item()/len(validation_datasets['valid_dataset'])
+    for i, (x0, composite_kspace, sense_map, acc_mask, data_consistency_masks, sub_slc_tf, index) in enumerate(validation_loader['valid_loader']):
+        with torch.no_grad():
+            x0                     = x0[0].to(device)                       # [K,Nx,Ny]
+            composite_kspace       = composite_kspace[0].to(device)         # [1,Nx,Ny,Nc]
+            sense_map              = sense_map[0].to(device)                # [2,Nx,Ny,Nc]
+            acc_mask               = acc_mask[0].to(device)                 # [Nx,Ny]
+            data_consistency_masks = data_consistency_masks[0].to(device)   # [Nx,Ny,K]
+            # Forward pass
+            loss = 0
+            for k in range(params['num_mask']):
+                loss_mask = acc_mask - data_consistency_masks[...,k]
+                xk0 = x0[2*k:2*k+2]   # x0 for kth DC mask
+                xt = xk0              # iteration starts with xt
+                for t in range(params['T']):
+                    L, zt = denoiser(xt[None,...])
+                    xt = model.DC_layer(xk0,zt[0],L,sense_map,data_consistency_masks[...,k])
+                # loss calculation for kth mask
+                kspace_loss = sf.forward(xt, sense_map, loss_mask)
+                # loss calculation
+                loss += L1L2Loss(kspace_loss, composite_kspace*loss_mask[None,...,None])/params['num_mask']
+                
+            loss_arr_valid[epoch] += loss.item()/len(validation_datasets['valid_dataset'])
         
     if ((epoch+1)%5==0):
         torch.save(denoiser.state_dict(), 'OVS_multimaskSSDU_' + f'{epoch+1:03d}'+ '.pt')
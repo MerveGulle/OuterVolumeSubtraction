66693e5598edcda9f89a1a975afc7156520a7c72
Merve Gulle <glle0001@umn.edu>
2022-11-14 16:44:13 -0600

    feat: Configure the device

--- MultiMaskSSDU_kspace_full/test.py
+++ MultiMaskSSDU_kspace_full/test.py
@@ -1,9 +1,14 @@
+import torch
+import SupportingFunctions as sf
+
+
 ### HYPERPARAMETERS
 params = dict([('num_epoch', 100),
                ('batch_size', 1),
                ('learning_rate', 1e-3),
                ('num_training_slice', 'all'),
                ('num_validation_slice', 'all'),
+               ('num_test_slice', 'all'),
                ('num_workers', 0),          # It should be 0 for Windows machines
                ('use_cpu', False),
                ('num_mask', 3),             # number of masks
@@ -13,3 +18,14 @@ params = dict([('num_epoch', 100),
 ### PATHS 
 # test_data_path = "C:\Codes\p006_OVS\OVS\MultiMaskSSDU_kspace_full\TrainDataset"
 test_data_path  = "/home/naxos2-raid12/glle0001/TestData/"
+
+# 1) Device configuration
+device = torch.device('cuda' if (torch.cuda.is_available() and (not(params['use_cpu']))) else 'cpu')
+
+
+
+
+
+
+
+
